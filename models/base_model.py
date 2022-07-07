3e6a8294ad16e3bf9dc170808e2ce606c4a8ddde
0806socrrn <socrrn@gmail.com>
2022-07-06 22:28:41 -0500

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,21 @@ Base models
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """
     class BaseModel
     """
-def __init__(self, **kwargs):
-        """
-        id: string
-        created_at: datetime
-        updated_at: datetime
-        """
-        self.id = str(uuid.uuid4())
-        self.updated_at = kwargs.get("updated_at", None)
-        self.created_at = kwargs.get("created_at", None)
 
-        current_date = datetime.isoformat(datetime.now())
+
+def __init__(self, **kwargs):
+    """
+    id: string
+    created_at: datetime
+    updated_at: datetime
+    """
+    self.id = str(uuid.uuid4())
+    self.updated_at = kwargs.get("updated_at", None)
+    self.created_at = kwargs.get("created_at", None)
+
+    current_date = datetime.isoformat(datetime.now())
